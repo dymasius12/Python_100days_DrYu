890288c32567372c25d2820c76bd122dc6306471
Dymasius Sitepu <dymasius@Dymasiuss-MacBook-Air.local>
2022-09-24 23:10:26 +0800

    day 1 and day 2 100 day

---
+++ day1.py
@@ -0,0 +1,5 @@
+#!/opt/anaconda3/bin/python
+
+print("Day 1 - Python Print Function")
+print("the function..")
+print("print('printing function')")
---
+++ day2.py
@@ -0,0 +1,5 @@
+#!/opt/anaconda3/bin/python
+
+word = input("Input a word to count:")
+
+print(len(word))
